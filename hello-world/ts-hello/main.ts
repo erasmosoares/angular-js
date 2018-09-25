d7226e87d99412618750c6fa4e6e5f1d327bd33a
Erasmo Soares <esaj@cesar.org.br>
2018-09-25 12:52:30 -0300

    ts class

--- hello-world/ts-hello/main.ts
+++ hello-world/ts-hello/main.ts
@@ -5,3 +5,42 @@ let d: any;
 
 enum Color{Red = 0,Green = 1,Blue = 2};
 let BackgroundColor = Color.Blue;
+
+let message;
+message = 'abc';
+let endsWithC = (message as string).endsWith('c');
+
+//Arrow functions (lambda expression)
+let log = function(message){
+    console.log(message);
+}
+
+let arrowLog = (message) =>{
+    console.log(message);
+}
+
+//Interface
+interface Point{
+    a:number,
+    b:number,
+}
+
+let drawPoint = (point:Point)=>{
+    //..
+}
+
+class Point{
+
+    constructor(private _x?:number, private _y?:number){ }
+
+    get X(){
+        return this._x;
+    }
+
+    set X(value){
+        this._x = value;
+    }
+    draw(){
+
+    }
+}
