c4e6610d3979621a362739edfa8a4f4e2f610d8e
Erasmo Soares <esaj@cesar.org.br>
2018-10-15 14:49:11 -0300

    Drop-down list

--- hello-world/src/app/contact-form/contact-form.component.ts
+++ hello-world/src/app/contact-form/contact-form.component.ts
@@ -7,7 +7,10 @@ import { Component } from '@angular/core';
 })
 export class ContactFormComponent {
 
-  constructor() { }
+  contactMethods = [
+    {id:1,name:'Email'},
+    {id:2,name:'Phone'},
+  ];
 
   log(x){
     console.log(x);
