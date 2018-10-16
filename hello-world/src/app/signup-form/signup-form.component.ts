e984b9f45f961111aa799a73329249e028b681a1
Erasmo Soares de Araújo Júnior <erasmosaraujo@gmail.com>
2018-10-15 21:30:37 -0300

    custom validators

--- hello-world/src/app/signup-form/signup-form.component.ts
+++ hello-world/src/app/signup-form/signup-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators} from '@angular/forms';
+import { UsernameValidators } from './username.validators';
 
 @Component({
   selector: 'signup-form',
@@ -10,7 +11,8 @@ export class SignupFormComponent {
   form = new FormGroup({
     username: new FormControl('',[
       Validators.required,
-      Validators.minLength(3)]),
+      Validators.minLength(3),
+      UsernameValidators.cannotContainSpace]),
     password: new FormControl('',Validators.required),
   });
 
