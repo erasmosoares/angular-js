e70d2d04994f0f79c568c3274696a0d0fcb2dd59
Erasmo Soares de Araújo Júnior <erasmosaraujo@gmail.com>
2018-10-25 21:11:19 -0300

    Query parameters

--- hello-world/src/app/github-followers/github-followers.component.ts
+++ hello-world/src/app/github-followers/github-followers.component.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { GithubService } from '../services/github.service';
 
@@ -10,10 +11,18 @@ export class GithubFollowersComponent implements OnInit {
 
   followers: any;
 
-  constructor(private service: GithubService) { }
+  constructor(private route: ActivatedRoute, 
+              private service: GithubService) { }
 
   
   ngOnInit(): void {
+    this.route.paramMap.subscribe(params =>{
+
+    });
+    this.route.queryParamMap.subscribe(params=>{
+      
+    });
+
     this.service.getAll()
     .subscribe(followers=> this.followers = followers);
   } 
